bb9d981a415c3842c69b2e36fe2bf1ed4fd7059a
Prakash Malla <mallaprakash593@gmail.com>
2026-01-27 10:01:05 +0530

    removed nuxt folder

--- frontend/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "default" | "worker"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
