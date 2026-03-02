504a42b009ad3d04da40151f52cec7a7a797d185
Prakash Malla <mallaprakash593@gmail.com>
2026-03-02 17:23:35 +0530

    added cron

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -10,6 +10,13 @@
             class="px-1 px-sm-1 px-lg-1"
             max-width="1200"
           >
+          <v-alert
+            v-if="!isOnline"
+            type="warning"
+            class="mb-4"
+          >
+            You are currently offline. your data may not be saved until you go online.
+          </v-alert>
             <slot />
         </v-container>
       </v-main>
@@ -19,4 +26,19 @@
 
 <script setup lang="ts">
 import AppHeader from '~/components/common/AppHeader.vue';
+const isOnline = ref(navigator.onLine)
+
+function updateStatus() {
+  isOnline.value = navigator.onLine
+}
+
+onMounted(() => {
+  window.addEventListener('online', updateStatus)
+  window.addEventListener('offline', updateStatus)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('online', updateStatus)
+  window.removeEventListener('offline', updateStatus)
+})
 </script>
